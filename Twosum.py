988983bf675c59b975bf097fe0b97f4e66f9544f
Malavika Vinod <malavika.23bai10805@vitbhopal.ac.in>
2025-07-26 17:05:49 +0530

    Create Twosum.py

---
+++ Twosum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = {}  # Dictionary to store number -> index
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+
+        # This part should theoretically not be reached as per problem statement
+        # "You may assume that each input would have exactly one solution."
+        return []
